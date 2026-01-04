ddc32b6b0c10a8ea001780e43cd400f15c8868aa
TMadsky <245648606+TMadsky@users.noreply.github.com>
2026-01-04 09:11:14 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    printf("hello world");
+    printf("---------------------\n");
+    printf("Welcome To Calculator\n");
+    printf("---------------------\n");
+
+    printf("Enter First Number :");
+    float first;
+    scanf("%f", &first);
+
+    printf("Enter Second Number :");
+    float second;
+    scanf("%f", &second);
+
+
+
+    float Ad = first+second;
+    printf("Addition: %.1f", Ad);
+
+    float Sub = first-second;
+    printf("\nSubstraction: %.1f", Sub);
+
+    float Div = first/second;
+    printf("\nDividing: %.1f", Div);
+
+    float Mul = first*second;
+    printf("\nMultiplication: %.1f", Mul);
 }
