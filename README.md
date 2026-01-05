47f45ba0c426f7a04d3d5365c895ecaafc1e3cbe
TMadsky <245648606+TMadsky@users.noreply.github.com>
2026-01-05 18:09:49 +0300

    .

--- README.md
+++ README.md
@@ -2,6 +2,6 @@ Im gonna learn C from 0
 
 Day 1
     I made a calculator
-    
-Day 2
 
+Day 2
+    I made a Student System
--- StudentSystem.c
+++ StudentSystem.c
@@ -6,7 +6,7 @@ struct Student{
     int School_ID;
     char Name[30];
     int Age;
-    char Gender;
+    bool Gender;
 
 };
 
@@ -14,7 +14,7 @@ struct Teacher{
     int School_ID;
     char Name[30];
     int Age;
-    char Gender;
+    bool Gender;
     char Subject[40];
 };
 
@@ -28,7 +28,7 @@ void addStudent(struct Student *student){
     printf("Student Age:");
     scanf("%d", &student->Age);
 
-    printf("Student Gender:");
+    printf("Student Gender(1 Male 0 Female):");
     scanf(" %c", &student->Gender);
     printf("\n");
 }
@@ -49,7 +49,7 @@ void addTeacher(struct Teacher *teacher){
     scanf("%d", &teacher->Age);
     
 
-    printf("Teacher Gender:");
+    printf("Teacher Gender(1 Male 0 Female):");
     fflush(stdout);
     scanf(" %c", &teacher->Gender);
     
@@ -60,23 +60,33 @@ void addTeacher(struct Teacher *teacher){
 };
 
 int main(){
+    char gender;
+
     struct Student student1;
     struct Teacher teacher1;
 
     addStudent(&student1);
     addTeacher(&teacher1);
 
+    if(student1.Gender==1||teacher1.Gender==1){
+        char gender ="Male";
+    }
+    else{
+        char gender ="Female";
+    };
+
+
     printf("\n--- Student Information ---\n");
     printf("ID: %d\n", student1.School_ID);
     printf("Name: %s\n", student1.Name);
     printf("Age: %d\n", student1.Age);
-    printf("Gender: %c\n", student1.Gender);
+    printf("Gender: %s\n", &gender);
 
     printf("\n--- Teacher Information ---\n");
     printf("ID: %d\n", teacher1.School_ID);
     printf("Name: %s\n", teacher1.Name);
     printf("Age: %d\n", teacher1.Age);
-    printf("Gender: %c\n", teacher1.Gender);
+    printf("Gender: %c\n", &gender);
     printf("Subject: %s\n", teacher1.Subject);
     return 0;
 }
