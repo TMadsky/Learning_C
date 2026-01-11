74a0695729a4895398e03a1bc086c11a5732e89e
TMadsky <245648606+TMadsky@users.noreply.github.com>
2026-01-11 08:17:26 +0300

    Update EverythingIdoneSoFarForFirstWeek.c

--- First Week/EverythingIdoneSoFarForFirstWeek.c
+++ First Week/EverythingIdoneSoFarForFirstWeek.c
@@ -1,4 +1,27 @@
 #include <stdio.h> 
-int main(){ 
+struct product{
+    int id;
+    char name;
+    float price;
+}
 
+void producta(struct product * product){
+    printf("Add Product");
+    printf("Product Id: ");
+    scanf("%d",&product -> id);
+
+    printf("Product Name: ");
+    scanf("%s",&product -> name);
+
+    printf("Product Price: ");
+    scanf("%f",&product -> price);
+}
+
+int main(){ 
+    struct product product1;
+    producta(&product1);
+
+    printf("Product Id :%d\n",product1.id);
+    printf("Product Name :%d\n",product1.name);
+    printf("Product Price :%d\n",product1.price);
 }
