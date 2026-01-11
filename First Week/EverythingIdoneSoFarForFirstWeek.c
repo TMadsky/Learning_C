2597b69c24e6ccda9271e5f09d9fd4a344eb4996
TMadsky <245648606+TMadsky@users.noreply.github.com>
2026-01-11 08:25:17 +0300

    Update EverythingIdoneSoFarForFirstWeek.c

--- First Week/EverythingIdoneSoFarForFirstWeek.c
+++ First Week/EverythingIdoneSoFarForFirstWeek.c
@@ -18,10 +18,34 @@ void producta(struct product * product){
 }
 
 int main(){ 
+    while(true){
+    int productCount = pC;   
+    printf("Press 1 for adding a new product\n");
+    printf("Press 2 for viewing products");
+    printf("Press 3 for exiting ");
+    int i
+    if(i=1){
     struct product product1;
     producta(&product1);
+
 
     printf("Product Id :%d\n",product1.id);
     printf("Product Name :%d\n",product1.name);
     printf("Product Price :%d\n",product1.price);
+    pC=pC+1;
+    }
+    if(i=2){
+    int arr[];    
+    printf("Product Id :%d\n",product1.id);
+    printf("Product Name :%d\n",product1.name);
+    printf("Product Price :%d\n",product1.price);
+    }
+    if(i=3){
+        break();
+    }
+    else{
+        printf("Unidentified Number Please Try Again\n");
+    }
+    }
+    
 }
